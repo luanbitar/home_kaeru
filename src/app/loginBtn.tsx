8928142fe5fc5208d52b8a027746e2be07ff149a
Luan Bitar <7@lbitar.com>
2024-02-25 15:30:58 -0300

    chore: removing unused code

--- src/app/loginBtn.tsx
+++
@@ -1,8 +0,0 @@
-import { getServerAuthSession } from "~/server/auth";
-import LoginBtn2 from "./loginbtn2";
-
-export default async function LoginBtn() {
-  const authSession = await getServerAuthSession();
-
-  return <LoginBtn2 authSession={authSession} />;
-}
